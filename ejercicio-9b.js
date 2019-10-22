c927c89528de2e47f3ea6e3d40fc92d106170236
CCOviedo <37302280+CCOviedo@users.noreply.github.com>
2019-10-22 18:30:19 +0200

    Add files via upload

---
+++ ejercicio-9b.js
@@ -0,0 +1,7 @@
+var diaSemana = "Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo";
+var ciudad = "Madrid.Sevilla.Barcelona.Valencia.Zaragoza.Bilbao.Donosti";
+
+diaSemana.split(",");
+ciudad.split(".");
+
+var listado = [diaSemana, ciudad];
